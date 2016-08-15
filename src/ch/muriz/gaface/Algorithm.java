0543e4b46a6fe2ef982a2fe39f87ffe19950e810
Murgio <muriz.se@gmail.com>
2016-08-15 21:27:17 +0200

    Type Mismatches are fixed

--- src/ch/muriz/gaface/Algorithm.java
+++ src/ch/muriz/gaface/Algorithm.java
@@ -18,31 +18,31 @@ public class Algorithm {
     public static Population evolvePopulation(Population pop) {
         fitness = new Fitness();
         Population newPopulation = new Population(false);
-        List<List<Double>> DNAList = pop.getDNAList();
+        List<List<Integer>> DNAList = pop.getDNAList();
         List<Double> fitnessList = new ArrayList<Double>();
-        for(List<Double> list : DNAList) {
+        for(List<Integer> list : DNAList) {
             try {
                 fitnessList.add(fitness.calculateFitness(list));
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-        List<List<Double>> individualFitness = new ArrayList<>();
-        List<Double> index = new ArrayList<Double>();
+        List<List<Number>> individualFitness = new ArrayList<>();
+        List<Integer> index = new ArrayList<Integer>();
         List<Double> fitness = new ArrayList<Double>();
-        List<Double> mergedList = new ArrayList<Double>();
+        List<Number> mergedList = new ArrayList<Number>();
         for(int i = 0; i < fitnessList.size(); i++) {
-            index.add((double)i);
+            index.add(i);
             fitness.add(fitnessList.get(i));
         }
-        for(double index1 : index) {
+        for(int index1 : index) {
             for(double index2 : fitness) {
                 mergedList.add(index2);
                 mergedList.add(index1);
             }
         }
         individualFitness = Utils.choppedList(mergedList, 2);
-        Collections.sort(individualFitness, new Comparator<List<Double>>() {
+        Collections.sort((individualFitness), new Comparator<List<Double>>() {
             public int compare(List<Double> list1, List<Double> list2) {
                 //Simple comparison here
                 return list1.get(0).compareTo(list2.get(0));
@@ -50,9 +50,9 @@ public class Algorithm {
         });
         Collections.reverse(mergedList);
         for(int i = 0; i < (size/2); i++) {
-            List<List<Double>> matingDNA = new ArrayList<>();
-            List<Double> matingPair = tournamentSelection(individualFitness);
-            for(double individualNumber : matingPair) {
+            List<List<Integer>> matingDNA = new ArrayList<>();
+            List<Number> matingPair = tournamentSelection(individualFitness);
+            for(Number individualNumber : matingPair) {
                 matingDNA.add(pop.getDNAList().get((int)individualNumber));
             }
 
@@ -83,7 +83,7 @@ public class Algorithm {
             }
 
             // Add a new individual based on the newly mutated/crossed over DNA to the population
-            for(List<Double> DNA : matingDNA) {
+            for(List<Integer> DNA : matingDNA) {
                 individuals.add(new Individual(DNA));
             }
         }
@@ -95,13 +95,13 @@ public class Algorithm {
      * Based on a list of (fitness, individualNumber) tuples
      * Uses tournament selection
      */
-    private static List<Double> tournamentSelection(List<List<Double>> individualFitness) {
+    private static List<Number> tournamentSelection(List<List<Number>> individualFitness) {
         int tournamentSize = (int)Math.ceil(individualFitness.size() * Settings.TOURNAMENT_FRACTION);
         if(tournamentSize == 1) tournamentSize = 2;
 
         // Create tournament
         int listCount = individualFitness.size();
-        List<List<Double>> tournament = new ArrayList<>();
+        List<List<Number>> tournament = new ArrayList<>();
         while(tournament.size() < tournamentSize) {
             int chosenIndividual = rand.nextInt(listCount);
             if(!tournament.contains(individualFitness.get(chosenIndividual))) {
@@ -116,9 +116,9 @@ public class Algorithm {
             }
         });
         Collections.reverse(tournament);
-        List<Double> floatList1 = tournament.get(0);
-        List<Double> floatList2 = tournament.get(1);
-        List<Double> resultList = new ArrayList<>();
+        List<Number> floatList1 = tournament.get(0);
+        List<Number> floatList2 = tournament.get(1);
+        List<Number> resultList = new ArrayList<>();
         resultList.add(floatList1.get(1)); resultList.add(floatList2.get(1));
         return resultList;
     }
@@ -126,20 +126,20 @@ public class Algorithm {
     /*
      * Crosses over two pieces of mating DNA
      */
-    private static List<List<Double>> crossover(List<List<Double>> matingDNA) {
+    private static List<List<Integer>> crossover(List<List<Integer>> matingDNA) {
         int pivot = rand.nextInt(matingDNA.get(0).size());
-        List<List<Double>> result = new ArrayList<>();
-        List<Double> firstMatingDNA = matingDNA.get(0);
-        List<Double> secondMatingDNA = matingDNA.get(1);
-        List<Double> firstPart = firstMatingDNA.subList(0, firstMatingDNA.size()-pivot);
-        List<Double> secondPart = secondMatingDNA.subList(secondMatingDNA.size()-pivot, secondMatingDNA.size()+1);
-        ArrayList<Double> firstResult = new ArrayList<Double>(firstPart);
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> firstMatingDNA = matingDNA.get(0);
+        List<Integer> secondMatingDNA = matingDNA.get(1);
+        List<Integer> firstPart = firstMatingDNA.subList(0, firstMatingDNA.size()-pivot);
+        List<Integer> secondPart = secondMatingDNA.subList(secondMatingDNA.size()-pivot, secondMatingDNA.size()+1);
+        ArrayList<Integer> firstResult = new ArrayList<Integer>(firstPart);
         firstResult.addAll(secondPart);
         result.add(firstResult);
 
-        List<Double> firstPartList2 = secondMatingDNA.subList(0, secondMatingDNA.size()-pivot);
-        List<Double> secondPartList2 = firstMatingDNA.subList(firstMatingDNA.size()-pivot, firstMatingDNA.size()+1);
-        ArrayList<Double> secondResult = new ArrayList<Double>(firstPartList2);
+        List<Integer> firstPartList2 = secondMatingDNA.subList(0, secondMatingDNA.size()-pivot);
+        List<Integer> secondPartList2 = firstMatingDNA.subList(firstMatingDNA.size()-pivot, firstMatingDNA.size()+1);
+        ArrayList<Integer> secondResult = new ArrayList<Integer>(firstPartList2);
         secondResult.addAll(secondPartList2);
         result.add(secondResult);
         return result;
@@ -148,10 +148,10 @@ public class Algorithm {
     /*
      * Randomly mutates a piece of DNA
      */
-    private static List<Double> mutate(List<Double> DNA) {
+    private static List<Integer> mutate(List<Integer> DNA) {
         int chosenBase = rand.nextInt(DNA.size());
         int chosenBaseType = Settings.INDIVIDUAL_BASE_TYPES[rand.nextInt(Settings.INDIVIDUAL_BASE_TYPES.length)];
-        DNA.set(chosenBase, (double)chosenBaseType);
+        DNA.set(chosenBase, chosenBaseType);
         return DNA;
     }
 }
